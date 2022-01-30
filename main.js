3e330f133155a08dbe30a0cddfec6304f34bd41a
kssrishi <92584456+kssrishi@users.noreply.github.com>
2022-01-30 17:36:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,23 +18,24 @@ canvas.addEventListener("mousemove", my_mousemove);
 function my_mousemove(e) {
   current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
   current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-  mouseEvent = "mouseMove";
+
   if (mouseEvent == "mouseDown") {
-    console.log("Last position of x and y coordinates = ");
-    console.log(
-      "x = " + current_position_of_x + "y = " + current_position_of_y
-    );
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = width_of_line;
-    ctx.arc(
-      current_position_of_mouse_x,
-      current_position_of_mouse_y,
-      radius,
-      0,
-      2 * Math.PI
+
+    console.log("Last position of x and y coordinates = ");
+    console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+    
+
+    console.log("Current position of x and y coordinates = ");
+    console.log(
+      "x  = " +
+        current_position_of_mouse_x +
+        "y = " +
+        current_position_of_mouse_y
     );
-    ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+    ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2 * Math.PI);
     ctx.stroke();
   }
 
